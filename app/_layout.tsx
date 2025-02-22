0c8651b99a96f6d57410a6946b66e1ef939fd271
tzniceguy <joeshimbi@gmail.com>
2025-02-23 01:19:44 +0300

    adding (screens) to router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(screens)" />
+    </Stack>
+  );
 }
