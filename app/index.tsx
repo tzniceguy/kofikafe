5ce84730426771665a6de39f451efb895d0b681f
tzniceguy <joeshimbi@gmail.com>
2025-02-23 00:35:30 +0300

    welcome screen intialization

---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  Text,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Pressable,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <ImageBackground
+      source={require("../assets/images/get-started.png")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Fall in love with Cofee with Blissfull Delight!
+        </Text>
+        <Text style={styles.description}>
+          welcome to our cozy coffee corner, where everycup is delightful for
+          you
+        </Text>
+        <Pressable style={styles.button} onPress={() => router.push("/menu")}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    paddingBottom: 60,
+    paddingHorizontal: 20,
+  },
+  heading: {
+    textAlign: "center",
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "white",
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  description: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "white",
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: "orange",
+    width: "100%",
+    padding: 12,
+    borderRadius: 5,
+    marginVertical: 20,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+});
