5b3a59640c36f4381b3cbade93bd60c84a77ff2d
tzniceguy <joeshimbi@gmail.com>
2025-02-23 13:42:51 +0300

    header component initialization

---
+++ components/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { ArrowLeft, Heart } from "lucide-react-native";
+import { useRouter } from "expo-router";
+export default function Header() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.header}>
+      <Pressable onPress={() => router.back()}>
+        <ArrowLeft size={24} color="#000" />
+      </Pressable>
+      <Text style={styles.headerText}>Product Detail</Text>
+      <Pressable>
+        <Heart size={24} color="#000" />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
