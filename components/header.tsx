736d1e32149469ad0e69af693ef44140d73f2262
tzniceguy <joeshimbi@gmail.com>
2025-02-25 21:23:13 +0300

    refactor: making the header component reusable by adding props

--- components/header.tsx
+++ components/header.tsx
@@ -2,18 +2,37 @@ import React from "react";
 import { View, Text, Pressable, StyleSheet } from "react-native";
 import { ArrowLeft, Heart } from "lucide-react-native";
 import { useRouter } from "expo-router";
-export default function Header() {
+
+interface HeaderProps {
+  title: string;
+  showFavoriteIcon?: boolean;
+  onFavoritePress?: () => void;
+}
+export default function Header({
+  title,
+  showFavoriteIcon = false,
+  onFavoritePress,
+}: HeaderProps) {
   const router = useRouter();
 
   return (
     <View style={styles.header}>
-      <Pressable onPress={() => router.back()}>
+      <Pressable onPress={() => router.back()} style={styles.backButton}>
         <ArrowLeft size={24} color="#000" />
       </Pressable>
-      <Text style={styles.headerText}>Product Detail</Text>
-      <Pressable>
-        <Heart size={24} color="#000" />
-      </Pressable>
+
+      {title && <Text style={styles.headerText}>{title}</Text>}
+
+      <View style={styles.rightIcon}>
+        {showFavoriteIcon ? (
+          <Pressable onPress={onFavoritePress}>
+            <Heart size={24} color="#000" />
+          </Pressable>
+        ) : (
+          // Empty View to maintain the space-between layout even when no right icon
+          <View style={styles.placeholder} />
+        )}
+      </View>
     </View>
   );
 }
@@ -30,5 +49,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "center",
+    flex: 1,
+  },
+  backButton: {
+    width: 40,
+  },
+  rightIcon: {
+    width: 40,
+    alignItems: "flex-end",
+  },
+  placeholder: {
+    width: 24,
+    height: 24,
   },
 });
