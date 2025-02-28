7a82248be082161df3bc48f0e3f1c687fdab22f4
tzniceguy <joeshimbi@gmail.com>
2025-03-01 01:41:59 +0300

    refactor: using individual stack to customize modal properties

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,42 @@
 import { Stack } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { X } from "lucide-react-native";
 
 export default function RootLayout() {
+  const router = useRouter();
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(modals)" options={{ presentation: "modal" }} />
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/map"
+        options={{
+          presentation: "modal",
+          headerTransparent: true,
+          title: "choose location",
+          animation: "fade",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <X size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="(modals)/add-note"
+        options={{
+          presentation: "modal",
+          headerTransparent: true,
+          title: "Add Note",
+          animation: "fade",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <X size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name="products/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
