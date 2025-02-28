8c855f90de744ab3accc45fac61cd21e0df96ce0
tzniceguy <joeshimbi@gmail.com>
2025-02-28 21:07:59 +0300

    feat: adding modals to the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(modals)" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
