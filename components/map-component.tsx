61c5b9ec2c38844e6ecfcf2cdfae5328bc956fad
tzniceguy <joeshimbi@gmail.com>
2025-02-28 10:24:53 +0300

    refactor: using google map as default map
    using google map as default map while adding focus region to dar es
    salaam.

--- components/map-component.tsx
+++ components/map-component.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function MapComponent() {
+  const region = {
+    latitude: -6.8235, // Dar es Salaam latitude
+    longitude: 39.2695, // Dar es Salaam longitude
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        provider="google"
+        initialRegion={region}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+      />
     </View>
   );
 }
